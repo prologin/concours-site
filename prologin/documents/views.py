5a0945bcd222398d6db68b6095efd6267b5eaacd
Jill-Jênn Vie <vie@jill-jenn.net>
2012-08-11 19:30:22 +0200

    Fixing paths. LaTeX needs to be updated.

--- prologin/documents/views.py
+++ prologin/documents/views.py
@@ -28,14 +28,14 @@ def gen_doc(request):
 def generate_convocations(request):
 	ct = request.GET.get('ct')
 	event_ids = request.GET.get('ids').split(',')
-	with open('prologin/documents/contestants.tex', 'w') as f:
+	with open('documents/contestants.tex', 'w') as f:
 		for event_id in event_ids:
 			e = Event.objects.get(id=event_id)
 			for c in e.contestant_set.all():
 				cp = c.user.get_profile()
 				f.write(latex_entities(u'\convocation{{{0}}}\n'.format('}{'.join([c.user.first_name, c.user.last_name, cp.address, cp.postal_code, cp.city, french_date(e.begin), e.center.name, e.center.address, e.center.postal_code + ' ' + e.center.city]))).encode('utf-8'))
 	os.system('pwd')
-	os.system('cd prologin/documents && pdflatex convocations-epreuves-regionales')
-	response = HttpResponse(FileWrapper(open('prologin/documents/convocations-epreuves-regionales.pdf')), mimetype='application/pdf')
+	os.system('cd documents && pdflatex convocations-epreuves-regionales')
+	response = HttpResponse(FileWrapper(open('documents/convocations-epreuves-regionales.pdf')), mimetype='application/pdf')
 	response['Content-Disposition'] = 'attachment; filename=convocations.pdf'
 	return response
