af2f53d2998ae3db3a64f1e91405c2bcfbe98478
Antoine Pietri <serialtueur@hotmail.fr>
2012-08-25 12:33:53 +0200

    Inconsistant use of tabs and spaces

--- prologin/problems/views.py
+++ prologin/problems/views.py
@@ -11,69 +11,69 @@ import datetime
 # Create your views here.
 
 def index(request):
-	return render_to_response('index.html')
+    return render_to_response('index.html')
 
 def get_props(filename):
-	return yaml.load(open(filename), Loader=yaml.loader.BaseLoader) # BaseLoader => 01 (sample) is not converted to int
+    return yaml.load(open(filename), Loader=yaml.loader.BaseLoader) # BaseLoader => 01 (sample) is not converted to int
 
 def path_problem_props(challenge, problem):
     return os.path.join(challenge, problem, 'problem.props')
 
 def get_challenge(path):
-	challenge = {}
-	problems = []
-	for item in os.listdir(path):
-		if '.svn' in item:
-			continue
-		subpath = '/'.join([path, item])
-		if item.endswith('.props'):
-			challenge['props'] = get_props(subpath)
-		elif os.path.isdir(subpath):
-			problems.append(get_problem(subpath))
-	return challenge, problems
+    challenge = {}
+    problems = []
+    for item in os.listdir(path):
+        if '.svn' in item:
+            continue
+        subpath = '/'.join([path, item])
+        if item.endswith('.props'):
+            challenge['props'] = get_props(subpath)
+        elif os.path.isdir(subpath):
+            problems.append(get_problem(subpath))
+    return challenge, problems
 
 def get_problem(path):
-	problem = {'name': os.path.basename(path)}
-	for item in os.listdir(path):
-		if '.svn' in item:
-			continue
-		subpath = '/'.join([path, item])
-		if item.endswith('.props'):
-			problem['props'] = get_props(subpath)
-		else:
-			problem.setdefault('list', []).append(item)
-	return problem
+    problem = {'name': os.path.basename(path)}
+    for item in os.listdir(path):
+        if '.svn' in item:
+            continue
+        subpath = '/'.join([path, item])
+        if item.endswith('.props'):
+            problem['props'] = get_props(subpath)
+        else:
+            problem.setdefault('list', []).append(item)
+    return problem
 
 def list_challenges(request):
-	challenges = []
-	for item in os.listdir('../../problems/'):
-		if '.svn' in item:
-			continue
-		props = u'../../problems/{0}/challenge.props'.format(item)
-		if os.path.exists(props):
-			challenges.append({'name': item, 'title': get_props(props)['title']})
-	return render_to_response('problems/index.html', {'challenges': challenges})
+    challenges = []
+    for item in os.listdir('../../problems/'):
+        if '.svn' in item:
+            continue
+        props = u'../../problems/{0}/challenge.props'.format(item)
+        if os.path.exists(props):
+            challenges.append({'name': item, 'title': get_props(props)['title']})
+    return render_to_response('problems/index.html', {'challenges': challenges})
 
 def list_problems(request, challenge):
-	challenge, problems = get_challenge('../../problems/{0}'.format(challenge))
-	problems = sorted(problems, key=lambda p: p['props']['difficulty'])
-	return render_to_response('problems/challenge.html', {'challenge': challenge, 'problems': problems})
+    challenge, problems = get_challenge('../../problems/{0}'.format(challenge))
+    problems = sorted(problems, key=lambda p: p['props']['difficulty'])
+    return render_to_response('problems/challenge.html', {'challenge': challenge, 'problems': problems})
 
 def show_problem(request, challenge, problem):
-	problem_path = '../../problems/{0}/{1}'.format(challenge, problem)
-	problem = get_problem(problem_path)
-	statement = open('{0}/subject.md'.format(problem_path)).read()
-	examples = []
-	print(problem['props'])
-	for test in problem['props']['samples'].split():
-		example = {}
-		for ext in ['in', 'comment', 'out']:
-			test_path = '{0}/{1}.{2}'.format(problem_path, test, ext)
-			print(test_path)
-			if os.path.exists(test_path):
-				example[ext] = open(test_path).read()
-		examples.append(example)
-	return render_to_response('problems/problem.html', {'problem': problem, 'statement': statement, 'examples': examples})
+    problem_path = '../../problems/{0}/{1}'.format(challenge, problem)
+    problem = get_problem(problem_path)
+    statement = open('{0}/subject.md'.format(problem_path)).read()
+    examples = []
+    print(problem['props'])
+    for test in problem['props']['samples'].split():
+        example = {}
+        for ext in ['in', 'comment', 'out']:
+            test_path = '{0}/{1}.{2}'.format(problem_path, test, ext)
+            print(test_path)
+            if os.path.exists(test_path):
+                example[ext] = open(test_path).read()
+        examples.append(example)
+    return render_to_response('problems/problem.html', {'problem': problem, 'statement': statement, 'examples': examples})
 
 def get_path_archive(challenge, problem, pseudo, timestamp):
     [match] = glob.glob(os.path.join(settings.ARCHIVES_PATH, '{0}-{1}-{2}-{3}.*'.format(timestamp, challenge, problem, pseudo)))
@@ -105,7 +105,7 @@ def trace(request, challenge, problem, timestamp):
     compilation, results, tests_details = vm_interface.parse_xml(xml, problem_props)
     tpl_env = {
         'compilation': compilation,
-	'results': results,
+        'results': results,
         'tests_details': tests_details,
         'challenge': challenge,
         'problem': problem,
