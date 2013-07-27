301285945b0a82313e82f17db2c77e31ed2008f0
Rodolphe Breard <rodolphe@what.tf>
2013-07-27 15:33:06 +0200

    rss links

--- prologin/prologin/settings.py
+++ prologin/prologin/settings.py
@@ -87,13 +87,21 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'ym9km85l4hk6f021zs2=s)3ewwltw0b^ck+3gjqc2v&afqa94y'
+SECRET_KEY = 'ym9km85l4hk6f021zs2=s)3ewwltw0b^ck+3gjqc2v&afqa94y' # CHANGEME
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
 )
 
 MIDDLEWARE_CLASSES = (
