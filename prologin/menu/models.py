088cda11105e9ae1ea26dbed07ea2d938edca640
Rodolphe Breard <rodolphe@what.tf>
2013-08-09 18:06:00 +0200

    fixing edit in the admin pannel

--- prologin/menu/models.py
+++ prologin/menu/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class MenuEntry(models.Model):
     name = models.CharField(max_length=32)
     url = models.CharField(max_length=120) # Do not change me to an URLField, I wouldn't be able to store relative URLs!
-    parent = models.ForeignKey('self', null=True)
+    parent = models.ForeignKey('self', null=True, blank=True, default=None)
     position = models.IntegerField()
     access_types = (
         ('all', 'All'),
