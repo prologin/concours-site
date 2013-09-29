6bef3202bfdeaa1175425ac59ec9e87f980b2d3d
Rodolphe Breard <rodolphe@what.tf>
2013-09-29 18:29:51 +0200

    menu now checks for access rights

--- prologin/menu/templatetags/menu_display.py
+++ prologin/menu/templatetags/menu_display.py
@@ -29,18 +29,32 @@ class MenuNode(Node):
             str_classes = ' class="%s"' % str_classes
         return '<li%s><a href="%s">%s</a></li>' % (str_classes, url, value)
 
+    def if_access(self, entry, context):
+        assoc = {
+            'all': lambda req : True,
+            'guest': lambda req : not req.user.is_authenticated(),
+            'logged': lambda req : req.user.is_authenticated(),
+            'admin': lambda req : req.user.is_staff,
+            }
+        return assoc[entry.access](context['request'])
+
     def render(self, context):
         ret = ''
         elems = MenuEntry.objects.all().filter(parent__id=None).order_by('position')
 
         for i in range(len(elems)):
             el = elems[i]
+            if not self.if_access(el, context):
+                continue
+
             children = MenuEntry.objects.all().filter(parent__id=el.id).order_by('position')
 
             if len(children) > 0:
                 children_html = ''
                 menu_class = 'menu-expanded' if self.tokens[0] == el.slug else 'menu-collapsed'
                 for child in children:
+                    if not self.if_access(child, context):
+                        continue
                     children_html += self.render_entry(get_real_url(child.url), real_value(child.name, context), ['menu-current'] if self.tokens[1] == child.slug else [])
                 ret += '<li class="%s"><span class="menu-main-elem">%s</span> <ul class="sub-menu">%s</ul></li>' % (menu_class, real_value(el.name, context), children_html)
             else:
