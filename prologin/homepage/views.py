2e2bc4662ba8f9eef1a5bb73557168dbc6f4be10
Alexandre `Zopieux` Macabies <web+git@zopieux.com>
2015-02-10 13:47:14 +0100

    Moved homepage CSS to its own folder; more links in jumbotron

--- prologin/homepage/views.py
+++ prologin/homepage/views.py
@@ -6,5 +6,6 @@ from zinnia.models import Entry
 def homepage(request):
     articles = Entry.published.all()[:settings.HOMEPAGE_ARTICLES]
     return render(request, 'homepage/homepage.html', {
+        'born_year': settings.PROLOGIN_EDITION - settings.PROLOGIN_MAX_AGE,
         'articles': articles,
     })
