83edd5062f9197ae3f432737b621eca0262506f1
Alexandre `Zopieux` Macabies <web+git@zopieux.com>
2015-03-08 19:22:56 +0100

    More fr→en i18n, more request.current_* variables.

--- prologin/prologin/middleware.py
+++ prologin/prologin/middleware.py
@@ -11,6 +11,9 @@ class ContestMiddleware(object):
         today = timezone.now().date()
         request.current_edition = contest.models.Edition.objects.prefetch_related('events').get(year=settings.PROLOGIN_EDITION)
         request.current_events = request.current_edition.events.filter(date_begin__lte=today, date_end__gte=today)
+        request.current_qualification = request.current_edition.events.filter(type=contest.models.Event.EventType.qualification.value).first()
+        request.current_regionales = request.current_edition.events.filter(type=contest.models.Event.EventType.regionale.value).order_by('date_begin')
+        request.current_finale = request.current_edition.events.filter(type=contest.models.Event.EventType.finale.value).first()
         request.current_qcm = qcm.models.Qcm.objects.filter(event__edition=request.current_edition).first()
         if user.is_authenticated():
             try:
