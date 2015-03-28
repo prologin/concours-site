8b12a9cf440c6de3841ace8b1baf41a73cf10c58
Alexandre `Zopieux` Macabies <web+git@zopieux.com>
2015-03-28 15:44:37 +0100

    Added some docstrings in utils.py

--- prologin/prologin/utils.py
+++ prologin/prologin/utils.py
@@ -35,8 +35,10 @@ def upload_path(*base_path):
     """
     Generate upload path for a FileInput.
     `base_path`: folder path of where to put the files
-    Examples:
-        upload_path('media', pictures')
+    Typical usage:
+        FileField(upload_to=upload_path('media', 'pictures'))
+    :param base_path: path of the directory (relative to MEDIA_ROOT) where to store the uploads
+    :rtype callable
     """
     parts = ['upload']
     parts.extend(base_path)
@@ -52,15 +54,38 @@ def upload_path(*base_path):
 class ChoiceEnum(enum.Enum):
     @staticmethod
     def tr(func):
+        """
+        Constructs a class that overloads label_for() to apply :param func on the label.
+        Typical usage:
+            class MyEnum(ChoiceEnum.tr(str.title), ChoiceEnum):
+                foo = 0
+                bar = 1
+                # Labels will be Foo and Bar.
+
+        :param func: the callable to apply on raw labels; the result will be passed to
+                     ugettext_lazy()
+        :rtype class
+        """
         label_for = classmethod(lambda cls, member: _(func(member.name)))
         return type('ChoiceEnumTr', (), {'label_for': label_for})
 
     @classmethod
     def label_for(cls, member):
+        """
+        Return the label for a given enum :param member.
+        :param member: one of the members of this enum
+        :return: (translated) string
+        """
         return _(member.name)
 
     @classmethod
     def choices(cls, empty_label=None):
+        """
+        Generate a tuple of pairs suitable for Django's `choices` model field kwarg.
+        :param empty_label: if provided, use this label instead of the default `-----`
+                            for the empty value
+        :rtype tuple of (db value, user facing string) pairs
+        """
         choices = tuple((m.value, cls.label_for(m)) for m in cls)
         if empty_label:
             choices = ((None, empty_label),) + choices
