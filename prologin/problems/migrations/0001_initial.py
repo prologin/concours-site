1b3c968813b31424a9e0ffdf10782e64b69cd18c
Alexandre `Zopieux` Macabies <web+git@zopieux.com>
2015-08-06 22:25:18 +0200

    Problem: add optional 'summary' on code submission (for versionning purposes)

--- prologin/problems/migrations/0001_initial.py
+++ prologin/problems/migrations/0001_initial.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
 from django.conf import settings
+from django.db import models, migrations
 
 import prologin.models
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
@@ -35,10 +36,11 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
                 ('language', prologin.models.CodingLanguageField(max_length=64, choices=[('ada', 'Ada'), ('brainfuck', 'Brainfuck'), ('c', 'C'), ('csharp', 'C#'), ('cpp', 'C++'), ('fsharp', 'F#'), ('haskell', 'Haskell'), ('java', 'Java'), ('js', 'Javascript'), ('lua', 'Lua'), ('ocaml', 'OCaml'), ('pascal', 'Pascal'), ('perl', 'Perl'), ('php', 'PHP'), ('pseudocode', 'Pseudocode'), ('python2', 'Python 2'), ('python3', 'Python 3'), ('scheme', 'Scheme'), ('vb', 'VB')], verbose_name='Coding language')),
                 ('code', models.TextField()),
+                ('summary', models.TextField(blank=True)),
                 ('score', models.IntegerField(null=True, blank=True)),
                 ('exec_time', models.IntegerField(null=True, blank=True)),
                 ('exec_memory', models.IntegerField(null=True, blank=True)),
-                ('date_submitted', models.DateTimeField(auto_now_add=True)),
+                ('date_submitted', models.DateTimeField(default=django.utils.timezone.now)),
                 ('submission', models.ForeignKey(to='problems.Submission', related_name='codes')),
             ],
             options={
@@ -47,6 +49,18 @@ class Migration(migrations.Migration):
                 'verbose_name_plural': 'Submission codes',
                 'get_latest_by': 'date_submitted',
             },
+        ),
+        migrations.CreateModel(
+            name='SubmissionCodeChoice',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('code', models.ForeignKey(related_name='submission_code_choices', to='problems.SubmissionCode')),
+                ('submission', models.ForeignKey(related_name='submission_choices', to='problems.Submission')),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='submissioncodechoice',
+            unique_together=set([('submission', 'code')]),
         ),
         migrations.AlterUniqueTogether(
             name='submission',
