24604af70fcf5c39433ece5ca685d38462521daf
Alexandre `Zopieux` Macabies <web+git@zopieux.com>
2015-10-16 23:05:07 +0200

    News: add legacy (by node id) URL redirect

--- prologin/news/urls.py
+++ prologin/news/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
+from news.views import LegacyUrlRedirectView
 
-urlpatterns = patterns('',
+
+urlpatterns = [
+    url(r'^legacy/(?P<pk>[0-9]+)$', LegacyUrlRedirectView.as_view()),
     url(r'^', include('zinnia.urls', namespace='zinnia')),
-)
+]
