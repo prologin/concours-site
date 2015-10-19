e59d0b42e1d6dd111c8fb163eedec32524cdcdbf
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2015-10-19 05:27:06 +0200

    problems: add . character to problems url patterns

--- prologin/problems/urls.py
+++ prologin/problems/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
         problems.views.LegacyChallengeRedirect.as_view(),
         name='legacy-challenge'),
 
-    url(r'^challenge/(?P<type>demi|qcm)(?P<year>[0-9]{4})/(?P<problem>[a-zA-Z0-9_-]+)$',
+    url(r'^challenge/(?P<type>demi|qcm)(?P<year>[0-9]{4})/(?P<problem>[a-zA-Z0-9_.-]+)$',
         problems.views.LegacyProblemRedirect.as_view(),
         name='legacy-problem'),
 
@@ -14,15 +14,15 @@ urlpatterns = [
         problems.views.Challenge.as_view(),
         name='challenge'),
 
-    url(r'^(?P<year>[0-9]{4})/(?P<type>[a-z\-]+)/(?P<problem>[a-zA-Z0-9_-]+)$',
+    url(r'^(?P<year>[0-9]{4})/(?P<type>[a-z\-]+)/(?P<problem>[a-zA-Z0-9_.-]+)$',
         problems.views.Problem.as_view(),
         name='problem'),
 
-    url(r'^(?P<year>[0-9]{4})/(?P<type>[a-z\-]+)/(?P<problem>[a-zA-Z0-9_-]+)/lang-template$',
+    url(r'^(?P<year>[0-9]{4})/(?P<type>[a-z\-]+)/(?P<problem>[a-zA-Z0-9_.-]+)/lang-template$',
         problems.views.AjaxLanguageTemplate.as_view(),
         name='ajax-language-template'),
 
-    url(r'^(?P<year>[0-9]{4})/(?P<type>[a-z\-]+)/(?P<problem>[a-zA-Z0-9_-]+)/(?P<submission>[0-9]+)$',
+    url(r'^(?P<year>[0-9]{4})/(?P<type>[a-z\-]+)/(?P<problem>[a-zA-Z0-9_.-]+)/(?P<submission>[0-9]+)$',
         problems.views.SubmissionCode.as_view(),
         name='submission'),
 
