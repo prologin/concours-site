f764cdc129f08c3be6bb5ee3a8dd2a9310befd61
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2015-10-20 03:05:11 +0200

    problems: search for code stubs in skeleton/

--- prologin/problems/models/problem.py
+++ prologin/problems/models/problem.py
@@ -267,8 +267,8 @@ class Problem:
 
     def _get_language_templates(self):
         templates = {}
-        for item in os.listdir(self.file_path()):
-            full_path = self.file_path(item)
+        for item in os.listdir(self.file_path('skeleton')):
+            full_path = self.file_path('skeleton', item)
             try:
                 base_name, ext = os.path.splitext(os.path.basename(full_path))
             except ValueError:
