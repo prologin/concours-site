3fbeaa6c42190e72aa47caa249bd24ada6097382
Alexandre `Zopieux` Macabies <web+git@zopieux.com>
2015-10-28 00:45:01 +0100

    problems: resume_submissions: implement --renew

--- prologin/problems/management/commands/resume_corrections.py
+++ prologin/problems/management/commands/resume_corrections.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand
-from problems.models import  SubmissionCode
+from django.db import transaction
+import celery
+
+from problems.models import SubmissionCode
 from problems.tasks import submit_problem_code
 
 
@@ -8,19 +11,30 @@ class Command(BaseCommand):
     help = "Find uncorrected submissions with a non-empty celery_task_id and schedule them"
 
     def add_arguments(self, parser):
-        parser.add_argument('--confirm', action='store_true', help="Do not ask for confirmation")
+        parser.add_argument('--renew', action='store_true', help="Renew the task ID")
+        parser.add_argument('--yes', action='store_true', help="Do not ask for confirmation")
+
+    def get_submissions(self):
+        return SubmissionCode.objects.filter(score__isnull=True).exclude(celery_task_id='')
 
     def handle(self, *args, **options):
-        submissions = SubmissionCode.objects.filter(score__isnull=True).exclude(celery_task_id='')
+        submissions = self.get_submissions()
 
         if not submissions:
             self.stderr.write("No uncorrected submissions found.")
             return
 
         self.stdout.write("Found {} uncorrected submissions.".format(len(submissions)))
-        if not (options['confirm'] or input("Do you want to schedule them? [y/n] ") == "y"):
+        if not (options['yes'] or input("Do you want to schedule them? [y/n] ") == "y"):
             self.stderr.write("Operation aborted.")
             return
 
-        for submission in submissions:
-            submit_problem_code.apply_async(args=[submission.pk], task_id=submission.celery_task_id, throw=False)
+        if options['renew']:
+            with transaction.atomic():
+                for submission in submissions:
+                    submission.celery_task_id = celery.uuid()
+                    submission.save()
+
+        for submission in self.get_submissions():
+            self.stdout.write('{}: {}'.format(submission.pk, submission.celery_task_id))
+            submit_problem_code.apply_async(args=[submission.pk], task_id=submission.celery_task_id)
