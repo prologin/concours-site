9fd0310bb886cdb0ea5a5c5644209a539c66ea8d
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2015-11-05 01:34:45 +0100

    team: /team default view

--- prologin/team/urls.py
+++ prologin/team/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from team import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
     url(r'^(?P<year>\d+)/$', views.index, name='year'),
 )
--- prologin/team/views.py
+++ prologin/team/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_list_or_404
 from team.models import TeamMember
 
 
-def index(request, year):
+def index(request, year=None):
+    if year is None:
+        year = request.current_edition.year
     team = get_list_or_404(TeamMember.objects.select_related('user', 'role'), year=year)
     return render(request, 'team/index.html', {
         'timeline': TeamMember.objects.values('year').distinct().order_by('-year'),
