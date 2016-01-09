511c54e32a41c6b26b23ecb74e7c3be7c617b292
Shir0kamii <shir0kamii@gmail.com>
2016-01-09 06:56:17 +0100

    Add a correction command to calculate the quiz score

---
+++ prologin/contest/management/commands/correction.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+from django.core.management.base import BaseCommand
+
+from contest.models import Contestant
+
+from contest.models import Edition
+from qcm.models import Answer
+
+
+class Command(BaseCommand):
+    help = "Calculate the score of the questionnary for each candidate"
+
+    def handle(self, *args, **options):
+        edition = Edition(year=date.today().year)
+        contestants = Contestant.complete_for_semifinal.all().filter(edition=edition)
+        nb_contestant = contestants.count()
+
+        for i, contestant in enumerate(contestants, 1):
+            answers = Answer.objects.all().filter(contestant=contestant)
+            is_correct = lambda answer: answer.is_correct
+            contestant.score_qualif_qcm = len(list(filter(is_correct, answers))) * 2
+            print("[{}/{}] {}: {} points".format(
+                i, nb_contestant, contestant.user, contestant.score_qualif_qcm
+            ))
+            contestant.save()
+
+
