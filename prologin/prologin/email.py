5b730ea531ae29ebac3eed95a0d80cf3e1dc8ca9
Remi Audebert <halfr@lse.epita.fr>
2016-01-10 17:16:57 +0100

    email: handle attachements

--- prologin/prologin/email.py
+++ prologin/prologin/email.py
@@ -1,5 +1,14 @@
 from djmail.template_mail import TemplateMail
 
 
-def send_email(template_name, to, context, **kwargs):
-    return TemplateMail(template_name).send(to, context, **kwargs)
+def send_email(template_name, to, context, attachements=[], **kwargs):
+    """Sends an email with a template.
+
+    >>> send_email('mailing/end_qualifications', 'joseph@marchand.xxx', {'user': u})
+    >>> send_email('mailing/qualified', 'joseph@marchand.xxx', {'user': u},
+    ...            ['convocation_marchandj.pdf'])
+    """
+    email = TemplateMail(template_name)
+    for attachement in attachements:
+        email.attach_file(attachement)
+    email.send(to, context, **kwargs)
