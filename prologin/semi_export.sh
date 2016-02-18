009a08b9f61353ca31d194460a1c8e3d1fdbeac4
multun <victor.collod@epita.fr>
2016-02-18 18:10:41 +0100

    site: add semifinal export wrapper script

---
+++ prologin/semi_export.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $(id -u) -eq 0 ]; then
+  echo -n "stopping website service..."
+  systemctl stop website
+  echo -e "\tDone !"
+  su prologin -c "pew in prologin-site sh $0 $1"
+  systemctl start website
+  exit 0
+fi
+
+export DJANGO_SETTINGS_MODULE=prologin.settings.semifinal
+echo -n "Dumping database..."
+cd `dirname $0`
+./manage.py semifinal_export $1
