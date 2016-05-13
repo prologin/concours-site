845204665f9a693a5e292d688da13668858f3a1c
Alexandre Macabies <web+git@zopieux.com>
2016-05-13 17:31:47 +0200

    marauder: fix migration naming & dependencies

--- prologin/marauder/migrations/0003_add_importance.py
+++ prologin/marauder/migrations/0003_add_importance.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('marauder', '0001_initial'),
+        ('marauder', '0002_add_within_timestamp'),
     ]
 
     operations = [
