400da1572cbdb3746dba4997c39c0300700153ae
Alexandre Macabies <web+git@zopieux.com>
2016-10-16 01:12:09 +0200

    schools: admin: add imported to filters

--- prologin/schools/admin.py
+++ prologin/schools/admin.py
@@ -4,7 +4,7 @@ import schools.models
 
 
 class SchoolAdmin(admin.ModelAdmin):
-    list_filter = ('approved', 'type', 'academy')
+    list_filter = ('approved', 'imported', 'type', 'academy')
     search_fields = ('name', 'acronym', 'uai')
     list_display = ('name', 'acronym', 'city', 'approved',
                     'total_contestants_count',
