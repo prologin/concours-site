1ff6e727385199263096f6472fcf5927507e223a
Alexandre Macabies <web+git@zopieux.com>
2016-10-15 22:45:44 +0200

    schools: admin: add search fields

--- prologin/schools/admin.py
+++ prologin/schools/admin.py
@@ -5,6 +5,7 @@ import schools.models
 
 class SchoolAdmin(admin.ModelAdmin):
     list_filter = ('approved', 'type', 'academy')
+    search_fields = ('name', 'acronym', 'uai')
     list_display = ('name', 'acronym', 'city', 'approved',
                     'total_contestants_count',
                     'current_edition_contestants_count')
