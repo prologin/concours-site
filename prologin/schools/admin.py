4d21d5584f135182120302dc41014fdec77ed7e1
Alexandre Macabies <web+git@zopieux.com>
2016-10-17 19:30:36 +0200

    schools: admin: use AdminOrderFieldsMixin; add Contestants inline

--- prologin/schools/admin.py
+++ prologin/schools/admin.py
@@ -3,15 +3,44 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.text import ugettext_lazy as _
 
+import contest.models
 import schools.models
+from prologin.utils.db import AdminOrderFieldsMixin
 
 
-class SchoolAdmin(admin.ModelAdmin):
+class SchoolContestantsInline(admin.TabularInline):
+    model = contest.models.Contestant
+    fields = ['username', 'full_name']
+    readonly_fields = fields
+    extra = 0
+
+    def has_add_permission(self, request):
+        return False
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('user')
+
+    def username(self, obj):
+        return obj.user.username
+
+    def full_name(self, obj):
+        return obj.user.get_full_name()
+
+    def get_view_on_site_url(self, obj=None):
+        return reverse('users:profile', args=[obj.user.pk])
+
+
+class SchoolAdmin(AdminOrderFieldsMixin, admin.ModelAdmin):
+    def current_edition_count(request):
+        return schools.models.School.func_current_edition_contestants_count(request.current_edition.year)
+
     list_filter = ('approved', 'imported', 'type', 'academy')
     search_fields = ('name', 'acronym', 'uai', 'city')
     list_display = ('name', 'acronym', 'city', 'approved',
-                    'total_contestants_count',
-                    'current_edition_contestants_count')
+                    'tcc', 'cecc')
+    annotations = [('tcc', schools.models.School.func_total_contestants_count, _("Total contestant count")),
+                   ('cecc', current_edition_count, _("Current edition contestant count"))]
+    inlines = [SchoolContestantsInline]
     actions = ['merge']
 
     def merge(self, request, queryset):
