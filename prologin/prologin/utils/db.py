f0bf36e13ea26b81cefa92e6e7d9d8867f611a74
Alexandre Macabies <web+git@zopieux.com>
2016-10-17 19:29:15 +0200

    utils: db: AdminOrderFieldsMixin

--- prologin/prologin/utils/db.py
+++ prologin/prologin/utils/db.py
@@ -1,4 +1,5 @@
 from django.db.models import Case, When, Value
+from django.utils.translation import ugettext_lazy
 
 
 class CaseMapping(Case):
@@ -17,3 +18,58 @@ class CaseMapping(Case):
         cases = (When(**{field: key, 'then': Value(value)})
                  for key, value in mapping)
         super().__init__(*cases, **kwargs)
+
+
+class AdminOrderFieldsMixin:
+    """
+    Mixin to automatically annotate() a ModelAdmin queryset with sortable
+    (computed) fields and add the corresponding getters, so it is possible
+    to sort on these fields.
+
+    Sample usage:
+
+    class FooAdmin(AdminOrderFieldsMixin, ModelAdmin):
+        list_display = ['some_name']
+        annotations = [
+            # a name              # an annotate()-compatible expr
+            ('related_obj_count', Count('related_objs')),
+            # or
+            ('related_obj_count', Count('related_objs'), "Human description"),
+        ]
+    """
+    annotations = []
+
+    def __init__(self, *args, **kwargs):
+        # monkey-patch the instance with the annotations
+        for annotation in self.get_annotations():
+            try:
+                name, func = annotation
+                text = ugettext_lazy(name.replace('_', ''))
+            except ValueError:
+                name, func, text = annotation
+
+            def method(obj):
+                return getattr(obj, name)
+            method.__qualname__ = name
+            method.admin_order_field = name
+            method.short_description = text
+
+            setattr(self, name, method)
+
+        super().__init__(*args, **kwargs)
+
+    def get_annotations(self):
+        return self.annotations
+
+    def get_queryset(self, request):
+        kwargs = {}
+        for annotation in self.get_annotations():
+            try:
+                name, func = annotation
+            except ValueError:
+                name, func, text = annotation
+            if callable(func):
+                func = func(request)
+            kwargs[name] = func
+
+        return super().get_queryset(request).annotate(**kwargs)
