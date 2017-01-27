2d2cecffe4dfe9d023573938199c35ce28476612
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2017-01-28 00:33:52 +0100

    problems: add migration to remove python2

---
+++ prologin/problems/migrations/0004_remove_python2.py
@@ -0,0 +1,23 @@
+import django.contrib.postgres.fields.jsonb
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('problems', '0003_replace_exec_stats_with_result'),
+    ]
+
+    operations = [
+        migrations.RunSQL('''
+            UPDATE problems_submissioncode
+            SET language = 'python'
+            WHERE language = 'python2' OR language = 'python3'
+        '''),
+
+        migrations.RunSQL('''
+            UPDATE contest_contestant
+            SET preferred_language = 'python'
+            WHERE preferred_language = 'python2' OR preferred_language = 'python3'
+        ''')
+    ]
