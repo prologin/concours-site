5db231dd5823340b5184bd0ecf34940d2d408761
Alexandre Macabies <web+git@zopieux.com>
2017-05-30 21:33:54 +0200

    tests: minor refactor & utility methods

--- prologin/prologin/settings/test.py
+++ prologin/prologin/settings/test.py
@@ -6,9 +6,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'prologin',
+        # 'USER': 'prologin',
+        # 'PASSWORD': 'foobar',
+        # 'TEST': {
+        #     'NAME': 'prologin_test',
+        # },
     },
 }
 
-PROLOGIN_EDITION = 2016
+PROLOGIN_EDITION = 2017
 RECAPTCHA_PUBLIC_KEY = ''
 RECAPTCHA_PRIVATE_KEY = ''
--- prologin/prologin/tests.py
+++ prologin/prologin/tests.py
@@ -1,16 +1,50 @@
+import contextlib
+import tidylib
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.http.response import HttpResponse
 from django.test import RequestFactory, TestCase
 from django.test.client import Client
 from django.utils import timezone
 
-import http.client
-import urllib.parse
-import time
-import re
-
 import contest.models
 import team.models
+
+
+class WithContestantMixin:
+    def _contribute(self):
+        super()._contribute()
+        self.contestant = self.create_user(
+            'contestant', 'contestant@example.com',
+            first_name="Joseph", last_name="Marchand", phone='0642424242')
+
+
+class WithStaffUserMixin:
+    def _contribute(self):
+        super()._contribute()
+        self.staff_user = self.create_user(
+            'staffer', 'staff@example.com',
+            is_staff=True)
+
+
+class WithOrgaUserMixin:
+    def _contribute(self):
+        super()._contribute()
+        self.orga_user = self.create_user('organizer', 'organizer@example.com')
+
+        self.orga_team_member = team.models.TeamMember(
+            user=self.orga_user,
+            year=self.edition_year,
+            role_code=team.models.Role.president.name)
+        self.orga_team_member.save()
+
+
+class WithSuperUserMixin:
+    def _contribute(self):
+        super()._contribute()
+        self.super_user = self.create_user(
+            'superuser', 'superuser@example.com',
+            is_staff=True, is_superuser=True)
 
 
 class ProloginTestCase(TestCase):
@@ -19,123 +53,62 @@ class ProloginTestCase(TestCase):
     Sets up a decent environment with required database entries as well as few
     test users.
     """
+    edition_year = settings.PROLOGIN_EDITION
 
     def setUp(self):
-        self.validator = Validator()
         self.client = Client()
         self.factory = RequestFactory()
 
-        self._make_edition()
-        self._make_users()
+        self._contribute()
 
-    def _make_users(self):
-        self.contestant = self.create_user(
-            'contestant', 'contestant@example.com', 'password')
-        self.organizer = self.create_user('organizer', 'organizer@example.com',
-                                          'password')
-
-        self.organizer_tm = team.models.TeamMember()
-        self.organizer_tm.user = self.organizer
-        self.organizer_tm.role_code = team.models.Role.president.name
-        self.organizer_tm.year = settings.PROLOGIN_EDITION
-        self.organizer_tm.save()
-
-    def _make_edition(self):
-        self.edition = contest.models.Edition()
-        self.edition.year = settings.PROLOGIN_EDITION
-        self.edition.date_begin = timezone.now()
-        self.edition.date_end = timezone.now()
+    def _contribute(self):
+        self.edition = contest.models.Edition(
+            year=settings.PROLOGIN_EDITION,
+            date_begin=timezone.now(),
+            date_end=timezone.now())
         self.edition.save()
 
-        self.finals = contest.models.Event()
-        self.finals.edition = self.edition
-        self.finals.type = contest.models.Event.Type.final.value
-        self.finals.save()
+        self.final_event = contest.models.Event(
+            edition=self.edition,
+            type=contest.models.Event.Type.final.value)
+        self.final_event.save()
 
-    def create_user(self, username, email, password, *args, **kwargs):
-        user = get_user_model().objects.create_user(username, email, password,
-                                                    *args, **kwargs)
+    @staticmethod
+    def create_user(username, email, password='password', *args, **kwargs):
+        user = get_user_model().objects.create_user(
+            username, email, password, *args, **kwargs)
         # Keep the original password for convenience.
         user.original_password = password
         return user
 
     def client_login(self, user):
-        return self.client.login(username=user.username,
-                                 password=user.original_password)
+        return self.client.login(
+            username=user.username,
+            password=user.original_password)
 
+    @contextlib.contextmanager
+    def user_login(self, user):
+        yield self.client_login(user)
+        self.client.logout()
 
-# TODO: Replace with a local validation library.
-class Validator:
-    def __init__(self):
-        self.useragent = (
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-            '(KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36')
+    def assertValidHTML(self, html):
+        doc, output = tidylib.tidy_document(html)
+        for line in output.strip().splitlines():
+            self.assertNotIn("Error", line, msg="invalid HTML: {}".format(line))
+            if 'trimming empty' in line:
+                continue
+            self.assertNotIn("Warning", line, msg="invalid HTML: {}".format(line))
 
-    def SOAPValidity(self, soap):
-        # Why not using a lib?
-        # 1. including dependencies only for unit testing sounds weird
-        # 2. at this time, there's no good and maintained projects.
-        pat = re.compile(r'^.*<m:validity>true</m:validity>.*$', re.M | re.I)
-        return pat.search(soap) != None
+    def assertValidResponse(self, response):
+        self.assertIsInstance(response, HttpResponse)
+        code = response.status_code
+        self.assertTrue(200 <= code < 300, msg="HTTP response should be valid but is {} {}".format(code, response.reason_phrase))
 
-    def wait(self):
-        """Pause the script so it doesn't flood the W3C Validation Service.
+    def assertInvalidResponse(self, response):
+        self.assertIsInstance(response, HttpResponse)
+        code = response.status_code
+        self.assertFalse(200 <= code < 300, msg="HTTP response should be erroneous but is {} {}".format(code, response.reason_phrase))
 
-        "Please be considerate in using this shared, free resource. Consider
-        Installing your own instance of the validator for smooth and fast
-        operation. Excessive use of the W3C Validation Service will be
-        blocked."
-        -- http://validator.w3.org/docs/api.html
-        """
-        time.sleep(1)
-
-    def checkHTML(self, html):
-        self.wait()
-        conn = http.client.HTTPConnection('validator.w3.org')
-        params = urllib.parse.urlencode({
-            'fragment': html,
-            'prefill': 0,
-            'doctype': 'Inline',
-            'prefill_doctype': 'html401',
-            'group': 0,
-            'output': 'soap12',
-        })
-        headers = {
-            'Content-type': 'application/x-www-form-urlencoded',
-            'Accept': 'text/plain,text/html,application/xhtml+xml,'
-                      'application/xml;q=0.9,*/*;q=0.8',
-            'User-Agent': self.useragent,
-        }
-        conn.request('POST', '/check', params, headers)
-        response = conn.getresponse()
-        if response.status != 200:
-            return False
-        data = response.read()
-        return self.SOAPValidity(str(data))
-
-    def checkCSS(self, css):
-        self.wait()
-        conn = http.client.HTTPConnection('jigsaw.w3.org')
-        params = urllib.parse.urlencode({
-            'text': css,
-            'profile': 'css3',
-            'usermedium': 'all',
-            'type': 'none',
-            'warning': '1',
-            'vextwarning': 'true',
-            'lang': 'en',
-            'output': 'soap12',
-        })
-        headers = {
-            'Accept': 'text/plain,text/html,application/xhtml+xml,'
-                      'application/xml;q=0.9,*/*;q=0.8',
-            'User-Agent': self.useragent,
-        }
-        conn.request('GET',
-                     '/css-validator/validator?' + str(params),
-                     headers=headers)
-        response = conn.getresponse()
-        if response.status != 200:
-            return False
-        data = response.read()
-        return self.SOAPValidity(str(data))
+    def assertResponseCode(self, response, code):
+        self.assertIsInstance(response, HttpResponse)
+        self.assertEqual(response.status_code, code, msg="HTTP response should be {} but is {} {}".format(code, response.status_code, response.reason_phrase))
