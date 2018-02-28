ba7db9086c2a1460542dea83df6a061e00b36932
Antoine Pietri <antoine.pietri1@gmail.com>
2018-02-28 14:11:38 +0100

    data: add a dump anonymization script

---
+++ data/anonymized_dump.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 dbname"
+    exit 1
+fi
+
+origdbname="$1"
+tmpdb="tmp_dump_$RANDOM"
+
+echo -n "Creating temporary database $tmpdb mirroring $origdbname... "
+createdb -T "$origdbname" "$tmpdb"
+echo "done."
+
+anonymize_field () {
+    tbl="$1"
+    field="$2"
+    replacement="${3:-"''"}"
+    echo -n "  anonymizing int field '$field'... "
+    echo "update $tbl set $field = $replacement;" | psql "$tmpdb"
+}
+
+# for digest()
+echo "create extension pgcrypto;" | psql --quiet "$tmpdb"
+
+# remove NOTICE messages
+export PGOPTIONS='--client-min-messages=warning'
+
+echo
+echo "Anonymizing table users_prologinuser"
+anonymize_field users_prologinuser password
+anonymize_field users_prologinuser first_name
+anonymize_field users_prologinuser last_name
+anonymize_field users_prologinuser email "encode(digest(username, 'sha1'), 'hex')"
+anonymize_field users_prologinuser address
+anonymize_field users_prologinuser postal_code
+anonymize_field users_prologinuser city
+anonymize_field users_prologinuser country
+anonymize_field users_prologinuser phone
+anonymize_field users_prologinuser gender 0
+anonymize_field users_prologinuser school_stage 0
+anonymize_field users_prologinuser birthday "'2000-01-01'"
+
+echo
+echo -n "Deleting all submission codes... "
+echo "truncate problems_submissioncode cascade" | psql "$tmpdb"
+
+dump="prologin-anonymized-dump-$( date --iso-8601 ).psql"
+echo
+echo -n "Dumping data to $dump... "
+pg_dump --no-owner --no-privileges "$tmpdb" > "$dump"
+echo "done."
+
+echo
+echo -n "Deleting temporary database $tmpdb... "
+dropdb $tmpdb
+echo "done."
