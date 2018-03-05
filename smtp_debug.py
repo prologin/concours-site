91e8e5cd0509c5f353fd517cc0e0e9f5ea87ea89
Alexandre Macabies <web+oss@zopieux.com>
2018-03-05 19:01:01 +0100

    dev: improve stmp_debug message printing

--- smtp_debug.py
+++ smtp_debug.py
@@ -8,8 +8,24 @@ class Server(smtpd.DebuggingServer):
         super().__init__(localaddr, remoteaddr)
         self.fake_lag = fake_lag
 
+    def _print_message_content(self, peer, data):
+        inheaders = True
+        lines = data.splitlines()
+        for line in lines:
+            # headers first
+            if inheaders and not line:
+                peerheader = 'X-Peer: ' + peer[0]
+                if not isinstance(peerheader, str):
+                    # decoded_data=false; make header match other binary output
+                    peerheader = peerheader.decode('utf-8', 'replace')
+                print(peerheader)
+                inheaders = False
+            if not isinstance(line, str):
+                line = line.decode('utf-8', 'replace')
+            print(line)
+
     def process_message(self, *args, **kwargs):
-        super(Server, self).process_message(*args, **kwargs)
+        super().process_message(*args, **kwargs)
         if self.fake_lag:
             time.sleep(self.fake_lag)
 
