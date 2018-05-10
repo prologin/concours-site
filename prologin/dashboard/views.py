2afb2b78dcaef44b9726dae321a73ac9f715f646
cafehaine <kilian.guillaume@gmail.com>
2018-05-10 23:54:12 +0000

    dashboard: add categories

--- prologin/dashboard/views.py
+++ prologin/dashboard/views.py
@@ -1,6 +1,4 @@
-import collections
-import io
-import json
+from collections import defaultdict
 import datetime
 
 from django.conf import settings
@@ -11,9 +9,9 @@ from rules.compat.access_mixins import PermissionRequiredMixin
 from django.db.models import Q
 
 from users.models import UserActivation
-from contest.models import Contestant, Edition
+from contest.models import Contestant, Edition, Assignation
 
-statusFetchers = []
+statusFetchers = defaultdict(lambda: [])
 
 class IndexView(PermissionRequiredMixin, TemplateView):
     permission_required = 'dashboard.view'
@@ -21,26 +19,47 @@ class IndexView(PermissionRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        statuses = [s() for s in statusFetchers]
-        context.update({'statuses': statuses})
+        context.update({'sections': statusFetchers})
         return context
 
-def status(f):
-    statusFetchers.append(f)
+class status(object):
+    def __init__(self, category):
+        self.category = category
 
-@status
+    def __call__(self, f):
+        statusFetchers[self.category].append(f)
+
+@status("Activations")
 def fetchExpiredUserActivations():
     objects = UserActivation.objects.filter(
             expiration_date__lt=datetime.datetime.now())
     return {'name':'Expired user activations', 'count':objects.count(),
             'detail':objects}
 
-@status
+@status("Activations")
+def fetchAwaitingUserActivations():
+    objects = UserActivation.objects.filter(
+            expiration_date__gte=datetime.datetime.now())
+    return {'name':'Awaiting user activations', 'count':objects.count(),
+            'detail':objects}
+
+@status("Contestants")
+def fetchUnassignedContestants():
+    contestants = Contestant.objects.filter(
+            Q(assignation_semifinal=Assignation.not_assigned.value) |
+            Q(assignation_final=Assignation.not_assigned.value))
+    return {'name':'Unassigned contestants', 'count':contestants.count(),
+            'detail':contestants}
+
+@status("Contestants")
 def fetchWeirdStates():
     # accepted in final but not in semi
-    contestants = Contestant.objects.filter(Q(assignation_semifinal=0)
-                                            | Q(assignation_semifinal=1),
-                                            assignation_final=2)
+    contestants = Contestant.objects.filter(
+            assignation_final=Assignation.assigned.value).exclude(
+                    assignation_semifinal=Assignation.assigned.value)
     return {'name':'Weird states', 'count':contestants.count(),
             'detail':contestants}
 
+# We disable the default_factory, otherwise the template engine of django won't
+# iterate over it.
+statusFetchers.default_factory = None
