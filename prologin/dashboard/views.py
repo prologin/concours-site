614df3e2b75a588a81b66f29c6a6ddf798520ba3
cafehaine <kilian.guillaume@gmail.com>
2018-05-10 22:42:34 +0000

    dashboard: add weird states, rework template

--- prologin/dashboard/views.py
+++ prologin/dashboard/views.py
@@ -1,14 +1,17 @@
 import collections
 import io
 import json
+import datetime
 
 from django.conf import settings
 from django.contrib import messages
 from django.core import serializers
 from django.views.generic import TemplateView
 from rules.compat.access_mixins import PermissionRequiredMixin
+from django.db.models import Q
 
 from users.models import UserActivation
+from contest.models import Contestant, Edition
 
 statusFetchers = []
 
@@ -26,11 +29,18 @@ def status(f):
     statusFetchers.append(f)
 
 @status
-def fetchUserActivations():
-    count = UserActivation.objects.count()
-    return {'name':'UserActivations','value':count,'status':count<5}
+def fetchExpiredUserActivations():
+    objects = UserActivation.objects.filter(
+            expiration_date__lt=datetime.datetime.now())
+    return {'name':'Expired user activations', 'count':objects.count(),
+            'detail':objects}
 
 @status
-def awaitingConfirmations():
-    return {'name':'yolo','value':42,'status':False}
+def fetchWeirdStates():
+    # accepted in final but not in semi
+    contestants = Contestant.objects.filter(Q(assignation_semifinal=0)
+                                            | Q(assignation_semifinal=1),
+                                            assignation_final=2)
+    return {'name':'Weird states', 'count':contestants.count(),
+            'detail':contestants}
 
