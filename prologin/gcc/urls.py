a6ccfdcd5d1a133243ed63ebad030c1d8e606afc
cafehaine <kilian.guillaume@gmail.com>
2018-08-01 19:54:15 +0200

    gcc: add basic template

--- prologin/gcc/urls.py
+++ prologin/gcc/urls.py
@@ -9,7 +9,15 @@ photos_patterns = [
     path('<int:year>/<int:edition>', views.PhotosEditionView.as_view(), name='photos_edition'),
 ]
 
+posters_patterns = [
+    path('', views.TeamIndexView.as_view(), name='team_index'),
+    path('<int:year>', views.TeamYearView.as_view(), name='team_year'),
+]
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('about', views.AboutView.as_view(), name='about'),
+    path('posters', views.PostersView.as_view(), name='posters'),
+    path('team/', include(posters_patterns)),
     path('photos/', include(photos_patterns)),
 ]
--- prologin/gcc/views.py
+++ prologin/gcc/views.py
@@ -4,12 +4,32 @@ from django.views.generic import TemplateView
 
 class PhotosIndexView(TemplateView):
     pass
+
 class PhotosYearView(TemplateView):
     pass
+
 class PhotosEditionView(TemplateView):
+    pass
+
+# Posters
+
+class PostersView(TemplateView):
+    pass
+
+# Team
+
+class TeamIndexView(TemplateView):
+    pass
+
+class TeamYearView(TemplateView):
+    pass
+
+# About
+
+class AboutView(TemplateView):
     pass
 
 # Homepage
 
 class IndexView(TemplateView):
-    pass
+    template_name="gcc/index.html"
