9d7f8b14c665114066448214465c3c1c2e78c1fa
Rémi Dupré <remim.dupre@gmail.com>
2018-09-16 14:46:42 +0200

    gcc: improve some model's display in the admin site

--- prologin/gcc/admin.py
+++ prologin/gcc/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from gcc.models import Edition, Event, Trainer, SubscriberEmail
 
-admin.site.register([Edition, Event, Trainer, SubscriberEmail])
+
+admin.site.register([Edition, SubscriberEmail])
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'can_view_applications')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('edition', 'center', 'event_start', 'event_end', 'signup_start', 'signup_end')
