d9fead392ded0ed6c66004a134812043e1927ace
Rémi Dupré <remim.dupre@gmail.com>
2018-09-16 14:46:42 +0200

    gcc: improve some model's display in the admin site

--- prologin/gcc/models.py
+++ prologin/gcc/models.py
@@ -15,6 +15,9 @@ class Edition(models.Model):
         """Gets the trainers who participate to this edition"""
         return Trainer.objects.filter(events__edition=self)
 
+    def __str__(self):
+        return str(self.year)
+
 
 class Event(models.Model):
     center = models.ForeignKey(Center, on_delete=models.CASCADE)
@@ -24,6 +27,9 @@ class Event(models.Model):
     signup_start = models.DateField(auto_now_add=True)
     signup_end = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.event_start) + ' ' + str(self.center)
+
 
 class Trainer(models.Model):
     events = models.ManyToManyField(Event)
@@ -31,6 +37,9 @@ class Trainer(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     can_view_applications = models.BooleanField(default=False)
     description = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
 
 
 class Application(models.Model):
@@ -42,6 +51,20 @@ class Application(models.Model):
 
     class Meta:
         unique_together = (('user', 'event'), )
+
+    def __str__(self):
+        if self.accepted:
+            status = 'accepted'
+        elif self.rejected:
+            status = 'rejected'
+        else:
+            status = 'pending
+
+        return '{} for {} ({})'.format(
+            str(self.user),
+            str(self.event),
+            status
+        )
 
 
 @ChoiceEnum.labels(str.capitalize)
