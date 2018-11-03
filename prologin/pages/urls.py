4799d7b5172481b5e1ebace6a75a5ffc936f9ee8
Thibault Allançon <tallancon@gmail.com>
2018-11-03 20:31:30 +0100

    prologin: add a 'contribute to Prologin' page

--- prologin/pages/urls.py
+++ prologin/pages/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('about/contest/history', views.AboutHistoryView.as_view(), name='about-history'),
     path('about/organization', views.AboutOrganizationView.as_view(), name='about-organization'),
     path('about/contest-rules', views.AboutContestRulesView.as_view(), name='about-contest-rules'),
+    path('about/contribute', views.AboutContributeView.as_view(), name='about-contribute'),
 ]
