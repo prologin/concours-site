969e667a1678763c10170217c4fec24ea0d85c15
Valérian Fayt <valerian.fayt@prologin.org>
2019-12-17 12:11:18 +0100

    sponsor: display sponsors in category

--- prologin/sponsor/views.py
+++ prologin/sponsor/views.py
@@ -11,7 +11,8 @@ class IndexView(TemplateView):
         sponsors = list(Sponsor.active.all())
 
         random.shuffle(sponsors)
-        sponsors.sort(key=lambda sponsor: sponsor.rank_significance, reverse=True)
 
-        context['sponsors'] = sponsors
+        context['sponsors_gold'] = [x for x in sponsors if x.rank_significance == 90]
+        context['sponsors_silver'] = [x for x in sponsors if x.rank_significance == 60]
+        context['sponsors_bronze'] = [x for x in sponsors if x.rank_significance == 30]
         return context
