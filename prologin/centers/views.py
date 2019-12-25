71529398595d7c420dc3725e84bcbda96a5b6dde
Valérian Fayt <valerian.fayt@prologin.org>
2019-12-25 19:17:24 +0100

    centers: rework absolute link

--- prologin/centers/views.py
+++ prologin/centers/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, RedirectView
+
+from django.conf import settings
+from django.urls import reverse
 
 import centers.models
 
@@ -11,3 +14,8 @@ class CenterListView(ListView):
     def get_queryset(self):
         return super().get_queryset().filter(type=centers.models.Center.Type.center.value,
                                              is_active=True)
+
+
+class CenterSpecificView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return f'{settings.SITE_BASE_URL}{reverse("centers:map")}#center-{self.kwargs["id"]}'
