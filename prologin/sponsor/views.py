ff656f763a89257a430e5d82e86d1781b9b9886e
Aurelien Rebourg <aurelien.rebourg@gmail.com>
2020-03-10 19:31:30 +0100

    sponsor: remove empty lines

--- prologin/sponsor/views.py
+++ prologin/sponsor/views.py
@@ -10,9 +10,7 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         sponsors = list(Sponsor.active.all())
-
         random.shuffle(sponsors)
-
         context['sponsors_gold'] = [x for x in sponsors if x.rank == "gold"]
         context['sponsors_silver'] = [x for x in sponsors if x.rank == "silver"]
         context['sponsors_bronze'] = [x for x in sponsors if x.rank == "bronze"]
