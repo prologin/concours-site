74642723824003524f520e458d9533711056f14d
Kenji Gaillac <kenji.gaillac@epita.fr>
2020-12-08 22:44:57 +0100

    docker-compose: use `set -e` instead of `&&` list

--- docker/docker_init.sh
+++ docker/docker_init.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-docker-compose build && \
-docker-compose up -d && \
+# Exit on error
+set -e
+
+docker-compose build
+docker-compose up -d
+
+# Build assets
 docker exec -it prologin_site sh -c '
 cd assets
 npm install
-' && \
-docker exec -it prologin_site python prologin/manage.py migrate && \
-docker exec -it prologin_site python prologin/manage.py collectstatic && \
-docker exec -it prologin_site python prologin/manage.py createsuperuser && \
+'
+
+docker exec -it prologin_site python prologin/manage.py migrate
+docker exec -it prologin_site python prologin/manage.py collectstatic
+
+docker exec -it prologin_site python prologin/manage.py createsuperuser
 docker exec -it prologin_site python prologin/manage.py edition create
