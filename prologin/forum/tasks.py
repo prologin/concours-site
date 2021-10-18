64c491c607cb2b4a80693e4bb46dc107775fee18
Léo Portemont <leo.portemont@epita.fr>
2021-10-18 21:28:11 +0200

    forum: fix notify url

--- prologin/forum/tasks.py
+++ prologin/forum/tasks.py
@@ -6,7 +6,7 @@ from celery import shared_task
 
 @shared_task(bind=True, ignore_result=True, default_retry_delay=60, max_retries=3)
 def notify_new_thread(self, thread: dict):
-    r = requests.post(settings.PROLOGIN_WEBHOOK_BASE_URL + '/django/forum',
+    r = requests.post(settings.PROLOGIN_WEBHOOK_BASE_URL + '/api/webhooks/prolosite/forum',
                       data=json.dumps(thread),
                       headers={'Authorization': settings.PROLOGIN_WEBHOOK_SECRET})
     if not r.ok:
