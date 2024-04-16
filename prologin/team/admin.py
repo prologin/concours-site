64cd0a14e8b991530c2ecfdf20274cdf508c09d4
El Laggron <laggron42@gmail.com>
2024-04-16 14:06:04 +0000

    team: add button to copy members
    Squashed commit of the following:
    
    * change to current year instead of next
    
    * team: add button to copy members

--- prologin/team/admin.py
+++ prologin/team/admin.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
+from django.conf import settings
+from django.contrib import admin, messages
 from django.utils.translation import ugettext_lazy as _
 
+from django.db.models import QuerySet
+
 import team.models
+from team.models import TeamMember
 
 
 class RoleFilter(admin.SimpleListFilter):
@@ -22,6 +26,7 @@ class TeamMemberAdmin(admin.ModelAdmin):
     list_display = ('user', 'year', 'role_significance', 'role_name', )
     raw_id_fields = ('user', )
     search_fields = ('user__first_name', 'user__last_name', 'user__username')
+    actions = ('copy_to_current_year', )
 
     def role_significance(self, member):
         return member.role_significance
@@ -35,5 +40,24 @@ class TeamMemberAdmin(admin.ModelAdmin):
     role_name.label = _("Role")
     role_name.admin_order_field = 'role_name_db'
 
+    def copy_to_current_year(self, request, queryset: "QuerySet[TeamMember]"):
+        year = settings.PROLOGIN_EDITION
+        i = 0
+        for member in queryset:
+            if TeamMember.objects.filter(user=member.user, year=year).exists():
+                continue
+            TeamMember.objects.create(user=member.user, role_code=member.role_code, year=year)
+            i += 1
+        if i > 0:
+            self.message_user(request, _("Copied %d team members to year %d") % (i, year))
+        else:
+            self.message_user(
+                request,
+                _("Selected team members already exist in year %d") % year,
+                level=messages.ERROR,
+            )
+    copy_to_current_year.short_description = _("Copy selected team members to current year")
+    copy_to_current_year.allowed_permissions = ["add"]
 
-admin.site.register(team.models.TeamMember, TeamMemberAdmin)
+
+admin.site.register(TeamMember, TeamMemberAdmin)
